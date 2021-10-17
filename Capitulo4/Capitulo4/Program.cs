84b8bdb810335981e57ac88063cdb0aca6d26178
Mauricio <mauriciovieira8@hotmail.com>
2021-10-17 17:04:05 -0300

    inclusão das anotações do capítulo 4 do livro

---
+++ Capitulo4/Capitulo4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Capitulo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] primeiraArray = new int[100]; //criando array e deixando espaços disponíveis
+            int[] segundoArray = new int[] { 1, 3, 5, 7, 9 }; //declarando array e atribuindo valores
+            int[] terceiroArray = { 1, 2, 3, 4, 5, 6 };
+
+            int[,] arrayMultidimensional = new int[2, 3]; //declarando array multidimensional
+            int[,] arrayMultidimensional2 = { { 1,2,3}, {4,5,6 } }; //array multidimensional e atribuindo valores
+
+            //declarando array e atribuindo valores
+            int[] segundArray = new int[] { 1, 3, 5, 7, 9 };
+            Console.WriteLine("Valor da Array na posição 0 -> {0} ", segundArray[0]);
+            Console.WriteLine("Valor da Array na posição 1 -> {0} ", segundArray[1]);
+            Console.WriteLine("Valor da Array na posição 1 -> {0} ", segundArray[2]);
+            Console.WriteLine("Valor da Array na posição 3 -> {0} ", segundArray[3]);
+            Console.WriteLine("Valor da Array na posição 4 -> {0} ", segundArray[4]);
+
+
+
+        }
+    }
+}
---
+++ Capitulo4/Lista/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nomesFuncionarios = new List<string>();
+            nomesFuncionarios.Add("Maria");
+            nomesFuncionarios.Add("João");
+            nomesFuncionarios.Add("André");
+            nomesFuncionarios.Add("Flávia");
+
+            foreach(string pessoa in nomesFuncionarios)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+           // Console.WriteLine(nomesFuncionarios[0]); //retorna a primeira da lista
+            Console.WriteLine("-------------------------------------");
+
+            nomesFuncionarios.RemoveAt(0); //removeu maria
+
+            Console.WriteLine(nomesFuncionarios[0]); //joão passou a ser o indexador 0
+            
+
+        }
+    }
+}
